5e0add6135ce4b585dc5c55ef216776e257a9ea7
Lazy-Rabbit-2001 <2733679597@qq.com>
2023-10-15 00:07:11 +0800

    preparation for players

--- classes/nodes/entities/class_entity_player_2d.gd
+++ classes/nodes/entities/class_entity_player_2d.gd
@@ -1,1 +1,11 @@
 class_name EntityPlayer2D extends EntityBody2D
+
+
+@export_group("General")
+@export_range(0, 3) var id: int
+@export var nickname: StringName = &"Player"
+@export_enum("Left: -1", "Right: 1") var direction: int = 1
+
+
+func _ready() -> void:
+	pass
--- lib_effect.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-@onready var icon: Sprite2D = $Icon
-
-
-func _ready() -> void:
-	pass
