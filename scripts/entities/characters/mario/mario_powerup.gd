2955a6519f0890dfa312ea88ca57c9e7a9a53b3f
Lazy-Rabbit-2001 <2733679597@qq.com>
2024-01-19 17:13:38 +0800

    made set_shapes_for_character() called deferred automatically

--- scripts/entities/characters/mario/mario_powerup.gd
+++ scripts/entities/characters/mario/mario_powerup.gd
@@ -25,19 +25,21 @@ func _powerup_exited() -> void:
 ## [br]
 ## [b]Note:[/b] This is done with [PhysicsServer2D]
 func set_shapes_for_character() -> void:
-	for i in collision_shapes:
-		if !i:
-			continue
-		var crid: RID = _character.get_rid()
-		var srid: RID = i.shape.get_rid() if i.shape else RID()
-		var index: int = collision_shapes.find(i)
-		# Adds shapes if there is no any shapes created for the character directly
-		if PhysicsServer2D.body_get_shape_count(crid) < collision_shapes.size():
-			PhysicsServer2D.body_add_shape(crid, srid, i.transform)
-		# Uses server to help with setting the shapes
-		# so that even the collision shapes are not the direct child of the character.
-		# the character still enjoys them as it should have them.
-		PhysicsServer2D.body_set_shape(crid, index, srid)
-		PhysicsServer2D.body_set_shape_as_one_way_collision(crid, index, i.one_way_collision, i.one_way_collision_margin)
-		PhysicsServer2D.body_set_shape_disabled(crid, index, i.disabled)
-		PhysicsServer2D.body_set_shape_transform(crid, index, i.transform)
+	(func() -> void:
+		for i in collision_shapes:
+			if !i:
+				continue
+			var crid: RID = _character.get_rid()
+			var srid: RID = i.shape.get_rid() if i.shape else RID()
+			var index: int = collision_shapes.find(i)
+			# Adds shapes if there is no any shapes created for the character directly
+			if PhysicsServer2D.body_get_shape_count(crid) < collision_shapes.size():
+				PhysicsServer2D.body_add_shape(crid, srid, i.transform)
+			# Uses server to help with setting the shapes
+			# so that even the collision shapes are not the direct child of the character.
+			# the character still enjoys them as it should have them.
+			PhysicsServer2D.body_set_shape(crid, index, srid)
+			PhysicsServer2D.body_set_shape_as_one_way_collision(crid, index, i.one_way_collision, i.one_way_collision_margin)
+			PhysicsServer2D.body_set_shape_disabled(crid, index, i.disabled)
+			PhysicsServer2D.body_set_shape_transform(crid, index, i.transform)
+	).call_deferred()
