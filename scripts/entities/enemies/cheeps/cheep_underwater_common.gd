fb624da2008db3d4280680987f38db354e747372
Lazy-Rabbit-2001 <2733679597@qq.com>
2024-03-17 16:24:42 +0800

    generator areas done, v0.1.1

--- scripts/entities/enemies/cheeps/cheep_underwater_common.gd
+++ scripts/entities/enemies/cheeps/cheep_underwater_common.gd
@@ -6,11 +6,8 @@ signal cheep_collided_wall ## Emitted when the cheep collides with wall.
 @export_category("Cheep Swimming")
 @export var fluid_group: StringName = &"water"
 @export var initially_facing_character: bool = true
-@export_group("References")
-@export_node_path("ShapeCast2D") var collider_path: NodePath = ^"Collider"
-@export_node_path("Area2D") var effect_box_path: NodePath = ^"EffectBox"
-@export_node_path("Timer") var timer_interval_path: NodePath = ^"TimerInterval"
 @export_group("Physics")
+@export_range(0, 1, 0.001, "or_greater", "hide_slider", "suffix:px") var body_radius: float = 16
 @export_enum("Linear", "Up Down", "Homing") var swimming_mode: int:
 	set(value):
 		swimming_mode = value
@@ -29,15 +26,23 @@ signal cheep_collided_wall ## Emitted when the cheep collides with wall.
 @export_range(0, 20, 0.001, "suffix:s") var swimming_up_down_interval: float = 1
 @export_subgroup("Homing", "homing_")
 @export_range(0, 60, 0.001, "suffix:s") var homing_interval: float = 1
+@export_group("Generated")
+@export var is_created_by_generator: bool
+@export var disappearance_margin: float = 32
+@export_group("References")
+@export_node_path("ShapeCast2D") var collider_x_path: NodePath = ^"ColliderX"
+@export_node_path("ShapeCast2D") var collider_y_path: NodePath = ^"ColliderY"
+@export_node_path("Area2D") var effect_box_path: NodePath = ^"EffectBox"
+@export_node_path("Timer") var timer_interval_path: NodePath = ^"TimerInterval"
 
 var velocity: Vector2
 var _local_velocity: Vector2
 
 var _delayed: int = 8
-var _normal: Vector2
 var _nearest_player: Character
 
-@onready var _collider: ShapeCast2D = get_node(collider_path)
+@onready var _collider_x: ShapeCast2D = get_node(collider_x_path)
+@onready var _collider_y: ShapeCast2D = get_node(collider_y_path)
 @onready var _effect_box: Area2D = get_node(effect_box_path)
 @onready var _timer_interval: Timer = get_node(timer_interval_path)
 
@@ -54,26 +59,19 @@ func _ready() -> void:
 			velocity = dir * swimming_speed.rotated(global_rotation)
 
 func _process(delta: float) -> void:
-	if _collider.is_colliding():
-		for i in _collider.get_collision_count():
-			_normal = _collider.get_collision_normal(i) # Get normal for turning back
-			
-			var col := _collider.get_collider(i)
-			if collidable && (!col is AreaFluid || (col is AreaFluid && !col.is_in_group(fluid_group))):
-				cheep_collided_wall.emit()
-	elif _normal.is_normalized(): # Turn back to prevent from going out of water
-		return_to_water() 
+	_update_collider(_collider_x)
+	_collision(_collider_x)
+	_update_collider(_collider_y)
+	_collision(_collider_y)
 	
 	global_position += velocity * delta
 	_local_velocity = velocity.rotated(-global_rotation)
-	_collider.position = 16 * _local_velocity.normalized()
 	
 	set_meta(&"facing", signf(_local_velocity.x))
 	
 	if _delayed > 0:
 		_delayed -= 1 # To prevent from mistaken detect as being out of water
 		return
-	
 	# If the cheep is indeed out of water, the notify that the fish is out of water currently
 	# And the signal should be connected to forced_kill() of EnemyKillingProcess
 	var is_in_fluid: bool = false
@@ -86,10 +84,34 @@ func _process(delta: float) -> void:
 		return
 
 
-func return_to_water() -> void:
-	if !_normal.is_normalized():
-		return
-	velocity = velocity.bounce(_normal)
+#region == Collider related ==
+func _update_collider(collider: ShapeCast2D) -> void:
+	if collider == _collider_x:
+		_collider_x.global_position = Vector2(global_position.x + body_radius * signf(velocity.x), global_position.y)
+		_collider_x.global_rotation = 0
+	if collider == _collider_y:
+		_collider_y.global_position = Vector2(global_position.x, global_position.y + body_radius * signf(velocity.y))
+		_collider_y.global_rotation = 0
+
+func _collision(collider: ShapeCast2D) -> void:
+	if collider.is_colliding():
+		for i in collider.get_collision_count():
+			var col := collider.get_collider(i)
+			if collidable && (!col is AreaFluid || (col is AreaFluid && !col.is_in_group(fluid_group))):
+				cheep_collided_wall.emit()
+	else: # Turn back to prevent from going out of water
+		return_to_water(collider)
+
+func return_to_water(collider: ShapeCast2D) -> void:
+	if collider == _collider_x:
+		velocity.x *= -1
+		_update_collider(_collider_x)
+		_collider_x.force_shapecast_update()
+	if collider == _collider_y:
+		velocity.y *= -1
+		_update_collider(_collider_y)
+		_collider_y.force_shapecast_update()
+#endregion
 
 
 func _on_interval() -> void:
@@ -101,3 +123,9 @@ func _on_interval() -> void:
 				velocity = Vector2.ZERO
 				return
 			velocity = swimming_speed.rotated(global_position.angle_to_point(_nearest_player.global_position))
+
+func _on_screen_exited() -> void:
+	_delayed = 8 # Refresh the delay to prevent mistaken death from being out of water
+	if !is_created_by_generator:
+		return
+	queue_free()
